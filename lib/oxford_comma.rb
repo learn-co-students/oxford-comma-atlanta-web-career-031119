73102c76ea020a8c7e856b741744b77b00c3048d
Darcy Linde <lindedarcy@gmail.com>
2019-02-22 20:08:14 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size < 2
+    array.join
+  elsif array.size == 2
+    last_element = array.pop
+    array.join(", ") + " and #{last_element}"
+  else
+    last_element = array.pop
+    array.join(", ") + ", and #{last_element}"
+  end
 end
