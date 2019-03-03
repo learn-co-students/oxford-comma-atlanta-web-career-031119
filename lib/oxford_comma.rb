1da5e7f0ff6f1697918038e6d925059f1d607bcb
Travis Martin <travisemerymartin@gmail.com>
2019-03-03 01:14:10 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.to_s
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length > 2
+    array.last = "and " + array.last
+    array.join(", ")
+  end
 end
